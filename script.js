b3e9e041b3148314bba07b2508bf47906bab5b65
bsherbrook <bsherbrook@gmail.com>
2023-03-17 13:23:20 -0400

    build bare bones, ready to start working now!

---
+++ script.js
@@ -0,0 +1,17 @@
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+const bookButton= document.getElementById("addBook");
+
+function addBook(){
+    console.log('hey');
+}
+
+bookButton.addEventListener('click', addBook);
