6e11e4a7e6920f955a02639a2b4242af3c7e0c6d
bsherbrook <bsherbrook@gmail.com>
2023-03-19 13:15:32 -0400

    build table to display new book submissions

--- script.js
+++ script.js
@@ -1,50 +1,61 @@
-const bookButton= document.getElementById("addBook");
-const formBox= document.getElementById("formBox");
-const submitButt= document.getElementById("submitButt");
-const bookInfo= document.getElementsByClassName('bookInfo');
-const form= document.querySelector('form');
-const title= document.getElementById('title');
-const author= document.getElementById('author');
-const pageNum= document.getElementById('pageNum');
-const readStatus= document.getElementById('readStatus');
-const bookShelf= document.getElementById('bookShelf');
+const bookButton = document.getElementById("addBook");
+const formBox = document.getElementById("formBox");
+const submitButt = document.getElementById("submitButt");
+const bookInfo = document.getElementsByClassName("bookInfo");
+const form = document.querySelector("form");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pageNum = document.getElementById("pageNum");
+const readStatus = document.getElementById("readStatus");
+const bookTable = document.getElementById("bookTable");
 let myLibrary = [];
 
 class Book {
   constructor(
-    title = 'Unknown',
-    author = 'Unknown',
-    pageNum = '0',
+    title = "Unknown",
+    author = "Unknown",
+    pageNum = "0",
     readStatus = false
   ) {
-    this.title = title
-    this.author = author
-    this.pages = pageNum
-    this.readStatus = readStatus
+    this.title = title;
+    this.author = author;
+    this.pageNum = pageNum;
+    this.readStatus = readStatus;
   }
 }
 
-function newBook(e){
-  e.preventDefault();  //write conditional to validate form before array push
-  if(title.value && author.value && pageNum.value){
-  myLibrary.push(new Book(title.value, author.value, pageNum.value, readStatus.checked))  
-  let lastBook= myLibrary[(myLibrary.length)-1];
-  bookShelf.innerHTML += `I have read ${lastBook.title} by ${lastBook.author}<br>`;
-  formBox.style.display= 'none';}
+function submitBook(event) {
+  event.preventDefault();
+  if (title.value && author.value && pageNum.value) {
+    myLibrary.push(
+      new Book(title.value, author.value, pageNum.value, readStatus.checked)
+    );
+    let lastBook = myLibrary[myLibrary.length - 1];
+    if (lastBook.readStatus === false) {
+      lastBook.readStatus = "Unread";
+    }
+    if (lastBook.readStatus === true) {
+      lastBook.readStatus = "Read";
+    }
+    bookTable.innerHTML += `<tr><td>${lastBook.title}</td><td>${lastBook.author}</td><td>${lastBook.pageNum}</td><td>${lastBook.readStatus}</td></tr>`;
+    formBox.style.display = "none";
+  }
 }
 
 function addBookToLibrary() {
   // do stuff here
 }
 
-function addBook(){
-  if(formBox.style.display==='block'){return};
-    title.value='';
-    author.value='';
-    pageNum.value='';
-    readStatus.checked=false;
-    formBox.style.display='block';
+function addBook() {
+  if (formBox.style.display === "block") {
+    return;
+  }
+  title.value = "";
+  author.value = "";
+  pageNum.value = "";
+  readStatus.checked = false;
+  formBox.style.display = "block";
 }
 
-submitButt.addEventListener('click', newBook)
-bookButton.addEventListener('click', addBook);
+submitButt.addEventListener("click", submitBook);
+bookButton.addEventListener("click", addBook);
